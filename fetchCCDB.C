6dae32e6df7c41cd3d650f77011fdc08d7a7c909
Luca Quaglia <luca.quaglia@cern.ch>
2024-06-26 23:37:55 +0200

    Added second period of Pb-Pb data taking

--- fetchCCDB.C
+++ fetchCCDB.C
@@ -10,7 +10,8 @@ void fetchCCDB() {
 
     //General path to add flexibility to the code + period name
     //string period = "LHC23_pass4_skimmed_QC1"; //pp skimmed QC data of 2023 pass 4
-    string period = "LHC23_PbPb_pass3_I-A11"; //Pb-Pb dataset - one of the two used for the analyses of Nazar
+    //string period = "LHC23_PbPb_pass3_I-A11"; //Pb-Pb dataset - one of the two used for the analyses of Nazar
+    string period = "LHC23_PbPb_pass3_fullTPC"; //Pb-Pb dataset - other dataset for the analyses of Nazar
     string globalPath = "/home/luca/cernbox/assegnoTorino/MIDefficiency/AO2D/"+period+"/";
 
     //Path for the .txt file of the run list of the period
